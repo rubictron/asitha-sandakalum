00c556a06e2ea8d527e84ad41ba5867e28af7f5f
rubictron <p.asithasandakelum@gmail.com>
2021-05-30 11:39:04 +0530

    nav-bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
-import {BrowserRouter as Router } from 'react-router-dom'
+import Navbar  from './components/Navbar'
+import { BrowserRouter as Router } from 'react-router-dom'
+
 function App() {
   return (
-    <h1>Hello RUBICTRON</h1>
+    <Router>
+      <Navbar/>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks
+} from './NavbarElements'
+const Navbar = props => {
+    return (
+        <div>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">RUBICTRON</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="home">Home</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="skills">Skills</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
+        </div>
+    )
+}
+
+Navbar.propTypes = {
+
+}
+
+export default Navbar
